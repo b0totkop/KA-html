460cb6f23d995cdbc3ff2ae1d4cc2030e7964c28
2020b - Tóth-Kalocsai Koppány <74898463+b0totkop@users.noreply.github.com>
2024-02-23 11:35:18 +0100

    Add files via upload

---
+++ fetch/js/script.js
@@ -0,0 +1,33 @@
+function fetchData(userid) {
+    fetch(`https://jsonplaceholder.org/users/?id=${userid}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(user => {
+            console.log(user);
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+}
+
+console.log(fetchData(3));
+
+function show(user) {
+    let ki = "<tr>"
+    ki =+ "<td>" + user.id + "</td>";
+    ki =+ "<td>" + user.firstname + "</td>";
+    ki =+ "<td>" + user.lastname + "</td>";
+    ki =+ "<td>" + user.email + "</td>";
+    ki =+ "<td>" + user.birthDate + "</td>";
+    ki =+ "<td>" + user.login.username + "</td>";
+    ki =+ "<td>" + user.address.stdeet + "</td>";
+    ki =+ "<td>" + user.phone + "</td>";
+    ki =+ "<td>" + user.website + "</td>";
+    ki =+ "<td>" + user.company.name + "</td>";
+    ki =+ "</tr>"
+    document.getElementById("table").innerHTML += ki;
+}
