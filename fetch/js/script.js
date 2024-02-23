1df309781a7da1a89e9613380f8f14c1f3245bbe
2020b - Tóth-Kalocsai Koppány <74898463+b0totkop@users.noreply.github.com>
2024-02-23 12:51:24 +0100

    Update script.js

--- fetch/js/script.js
+++ fetch/js/script.js
@@ -1,5 +1,6 @@
-function fetchData(userid) {
-    fetch(`https://jsonplaceholder.org/users/?id=${userid}`)
+function fetchData() {
+    let userid = parseInt(document.getElementById("input").value);
+    fetch(`https://jsonplaceholder.org/users/?id=` + userid)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -8,26 +9,25 @@ function fetchData(userid) {
         })
         .then(user => {
             console.log(user);
+            show(user);
         })
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error);
         });
 }
 
-console.log(fetchData(3));
-
 function show(user) {
     let ki = "<tr>"
-    ki =+ "<td>" + user.id + "</td>";
-    ki =+ "<td>" + user.firstname + "</td>";
-    ki =+ "<td>" + user.lastname + "</td>";
-    ki =+ "<td>" + user.email + "</td>";
-    ki =+ "<td>" + user.birthDate + "</td>";
-    ki =+ "<td>" + user.login.username + "</td>";
-    ki =+ "<td>" + user.address.stdeet + "</td>";
-    ki =+ "<td>" + user.phone + "</td>";
-    ki =+ "<td>" + user.website + "</td>";
-    ki =+ "<td>" + user.company.name + "</td>";
-    ki =+ "</tr>"
+    ki += "<td>" + user.id + "</td>";
+    ki += "<td>" + user.firstname + "</td>";
+    ki += "<td>" + user.lastname + "</td>";
+    ki += "<td>" + user.email + "</td>";
+    ki += "<td>" + user.birthDate + "</td>";
+    ki += "<td>" + user.login.username + "</td>";
+    ki += "<td>" + user.address.stdeet + "</td>";
+    ki += "<td>" + user.phone + "</td>";
+    ki += "<td>" + user.website + "</td>";
+    ki += "<td>" + user.company.name + "</td>";
+    ki += "</tr>"
     document.getElementById("table").innerHTML += ki;
 }
